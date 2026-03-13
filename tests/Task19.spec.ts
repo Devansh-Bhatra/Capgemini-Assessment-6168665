e03a2139b1bc96f016a3c6c692b4e28192e0b424
Devansh-Bhatra <devanshbhatra2614@gmail.com>
2026-03-13 10:04:45 +0530

    Add new files

---
+++ tests/Task19.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+test("justdial notification handling", async ({ browser }) => {
+
+    const context = await browser.newContext({
+        permissions: ["notifications"]
+    });
+
+    const page = await context.newPage();
+
+    await page.goto("https://www.justdial.com");
+
+    await page.waitForLoadState("load");
+
+    await page.locator('input[placeholder*="Search"]').fill("Restaurants");
+
+    // await page.waitForLoadState("load")
+
+    await page.keyboard.press("Enter");
+
+    await page.waitForTimeout(10000)
+
+});
